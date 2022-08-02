01b9176f3eb0f03ecdaf5bc4d1fad5886034339d
nuronemli <nuronemli@gmail.com>
2022-08-02 21:48:00 +0100

    added some changes

--- src/featureNur2/Test2.java
+++ src/featureNur2/Test2.java
@@ -4,5 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Created my new branch");
         System.out.println("Created new package and test 2 class ");
+
+
     }
 }
