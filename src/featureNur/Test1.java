b3253162245a6ec845b27bb5c2c0218ef6fc8e6b
nuronemli <nuronemli@gmail.com>
2022-08-02 21:21:22 +0100

    New class

---
+++ src/featureNur/Test1.java
@@ -0,0 +1,9 @@
+package featureNur;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello this is a test");
+        System.out.println("Nur- Group 22");
+    }
+}
+
